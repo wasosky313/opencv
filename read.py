0243c5a1ba6f3886782060e51de3fcedd2b738a1
c1321414 Andy Jimenez Reyes <c1321414@bb.br.com>
2022-01-21 18:36:34 -0300

    Read video with cv2

--- read.py
+++ read.py
@@ -1,6 +1,18 @@
 import cv2 as cv
 
-# Read image
-img = cv.imread('Resources\Photos\cat.jpg')
-cv.imshow('Cat', img)
-cv.waitKey(0)
+# # Read image
+# img = cv.imread('Resources\Photos\cat.jpg')
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
+
+# Read video
+capture = cv.VideoCapture('Resources\Videos\dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('VIDEO', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
